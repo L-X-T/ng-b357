97f9d62a66725b5b93fba770c9c42337794034aa
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2024-04-06 10:44:38 +0200

    Add 2025 workshop base

---
+++ src/app/shared/global.ts
@@ -0,0 +1,4 @@
+const useSecureApi = true;
+
+export const apiUrl = useSecureApi ? 'https://demo.angulararchitects.io/api/' : 'http://www.angular.at/api/';
+export const pattern = '[a-zA-ZäöüÄÖÜß ]*';
