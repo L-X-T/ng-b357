9a200062234fb0326347bc45699eef2bb6073c05
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2024-04-20 20:26:55 +0200

    06 Using a pipe instead of a method in the template

--- src/app/flights/flight-card/flight-card.component.ts
+++ src/app/flights/flight-card/flight-card.component.ts
@@ -1,34 +1,26 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  DoCheck,
-  ElementRef,
-  inject,
-  Input,
-  isDevMode,
-  OnChanges,
-  OnDestroy,
-  OnInit,
-} from '@angular/core';
+import { Component,DoCheck, ElementRef, inject, Input, OnChanges, OnDestroy, OnInit } from '@angular/core';
 
-import moment from 'moment';
+import { DatePipe } from '@angular/common';
 
 import { Flight } from '../../entities/flight';
 import { CityPipe } from '../../pipes/city.pipe';
 import { BlinkService } from '../../shared/blink.service';
+import { FlightDatePipe } from '../shared/pipes/flight-date.pipe';
 
 @Component({
   selector: 'app-flight-card',
-  imports: [CityPipe],
+  imports: [DatePipe, CityPipe, FlightDatePipe],
   templateUrl: './flight-card.component.html',
   styleUrl: './flight-card.component.scss',
   // changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FlightCardComponent implements OnChanges, OnInit, DoCheck, OnDestroy {
-  private debug = isDevMode();
+  private debug = false;
 
   @Input({ required: true }) item!: Flight;
   @Input() selected = false;
+
+  readonly datePipe = new DatePipe('en-US');
 
   private readonly blinkService = inject(BlinkService);
   private readonly elementRef = inject(ElementRef);
@@ -63,11 +55,6 @@ export class FlightCardComponent implements OnChanges, OnInit, DoCheck, OnDestro
       console.log(this.item);
       console.log('selected: ' + this.selected);
     }
-  }
-
-  getDate(item: Flight): string {
-    console.log('FlightCard - getDate() was called');
-    return moment(item.date).format('MM.DD.YYYY HH:mm');
   }
 
   blinkFirstChild(): void {
