ab9bfaaad4752236c716e1bd05e6eada4b231783
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2024-09-05 16:42:03 +0200

    13 Semantic HTML & Structure

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,7 +2,7 @@ import { Component, inject } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 
 @Component({
-  selector: 'app-navbar',
+  selector: '[app-navbar]',
   templateUrl: 'navbar.component.html',
 })
 export class NavbarComponent {
@@ -10,14 +10,17 @@ export class NavbarComponent {
 
   protected sidebarVisible = false;
 
-  protected sidebarToggle(): void {
+  protected onToggleSidebar(): void {
     const body = this.document.getElementsByTagName('body')[0];
+    const aside = this.document.getElementsByTagName('aside')[0];
 
     if (!this.sidebarVisible) {
       body.classList.add('nav-open');
+      aside.setAttribute('aria-expanded', 'true');
       this.sidebarVisible = true;
     } else {
       body.classList.remove('nav-open');
+      aside.setAttribute('aria-expanded', 'false');
       this.sidebarVisible = false;
     }
   }
