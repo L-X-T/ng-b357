L-X-T/ng-p3rf

06 CD OnPush Immutability

src/app/flights/flight-search/flight-search.component.ts
@@ -95,10 +95,10 @@ export class FlightSearchComponent {
       flightDate.setTime(flightDate.getTime() + this.TEN_MINUTES);
 
       // Mutable
-      this.flights[0].date = flightDate.toISOString();
+      // this.flights[0].date = flightDate.toISOString();
 
       // Immutable
-      // ?
+      this.flights[0] = { ...this.flights[0], date: flightDate.toISOString() };
     }
 
     // RxJS

L-X-T/ng-b357

src/app/flights/flight-search/flight-search.component.ts
@@ -111,10 +111,10 @@ export class FlightSearchComponent {
       flightDate.setTime(flightDate.getTime() + this.TEN_MINUTES);
 
       // Mutable
-      this.oldSchoolFlights[0].date = flightDate.toISOString();
+      // this.oldSchoolFlights[0].date = flightDate.toISOString();
 
       // Immutable
-      // ?
+      this.oldSchoolFlights[0] = { ...this.oldSchoolFlights[0], date: flightDate.toISOString() };
     }
 
     // RxJS