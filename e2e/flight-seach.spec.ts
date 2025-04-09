1cd3e4803487230c1d26822767c29ada3f692f3e
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2025-04-09 17:35:30 +0200

    Playwright A11y: Specify WCAG version

--- e2e/flight-seach.spec.ts
+++ e2e/flight-seach.spec.ts
@@ -5,7 +5,10 @@ test.describe('flight search a11y', () => {
   test('should not have any automatically detectable accessibility issues', async ({ page }) => {
     await page.goto('/flights/flight-search');
 
-    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+    const accessibilityScanResults = await new AxeBuilder({ page })
+      // https://www.deque.com/axe/core-documentation/api-documentation/#axe-core-tags
+      .withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa']) // EAA
+      .analyze();
 
     expect(accessibilityScanResults.violations).toEqual([]);
   });
