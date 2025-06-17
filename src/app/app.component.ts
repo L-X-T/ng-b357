20edc81d9bd50e3dd5c4d4c1ddf21cdc6da16147
Henrik Herbst Grubbe <henrikhgrubbe@gmail.com>
2025-06-17 14:34:46 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, effect, Signal, signal, WritableSignal } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { FooterComponent } from './footer/footer.component';
+import { JsonPipe } from '@angular/common';
 
 @Component({
-  imports: [RouterModule, SidebarComponent, NavbarComponent, FooterComponent],
+  imports: [RouterModule, SidebarComponent, NavbarComponent, FooterComponent, JsonPipe],
   selector: 'app-b357-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {}
+export class AppComponent {
+  protected readonly source: WritableSignal<{ foo: string }> = signal({ foo: 'bar' });
+  protected readonly computedTest: Signal<string> = computed(() => this.source().foo);
+
+  protected setSource(): void {
+    this.source.set({ foo: 'baz' });
+  }
+
+  protected updateSource(): void {
+    this.source.update((current) => {
+      current.foo = 'baz';
+      return current;
+    });
+  }
+
+  constructor() {
+    effect(() => console.log('source changed', this.source()));
+    effect(() => console.log('computed changed', this.computedTest()));
+  }
+}
